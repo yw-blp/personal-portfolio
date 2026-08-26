855513076170614a3909fca945c4e42760b7005c
Yuuki Watanabe <yuuki.watanabe@student.ateneo.edu>
2026-08-26 20:00:58 +0800

    implemented spotlight effect for grid bg

--- src/sections/Home/Home.jsx
+++ src/sections/Home/Home.jsx
@@ -1,37 +1,18 @@
 import { useRef } from 'react';
+import useRelativeMousePosition from '../../hooks/useRelativeMousePosition';
 import styles from './Home.module.css';
 
 function Home() {
-    const outerRef = useRef(null);
-    const gridOverlayRef = useRef(null);
-    const borderOverlayRef = useRef(null);
+    const innerRef = useRef(null);
 
-    const handleMouseMove = (e) => {
-        const rect = outerRef.current.getBoundingClientRect();
-        borderOverlayRef.current.style.setProperty('--mouse-x', `${e.clientX - rect.left}px`);
-        borderOverlayRef.current.style.setProperty('--mouse-y', `${e.clientY - rect.top}px`);
-        gridOverlayRef.current.style.setProperty('--mouse-x', `${e.clientX - rect.left}px`);
-        gridOverlayRef.current.style.setProperty('--mouse-y', `${e.clientY - rect.top}px`);
-    };
-
-    const handleMouseEnter = () => {
-        gridOverlayRef.current.dataset.active = 'true';
-    };
-
-    const handleMouseLeave = () => {
-        gridOverlayRef.current.dataset.active = 'false';
-    };
+    useRelativeMousePosition(innerRef);
 
     return (
-        <div ref={outerRef} className={styles.outer} onMouseMove={handleMouseMove}>
-            <div
-                className={styles.inner}
-                onMouseEnter={handleMouseEnter}
-                onMouseLeave={handleMouseLeave}
-            >
+        <div className={styles.outer}>
+            <div ref={innerRef} className={styles.inner}>
                 <div className={styles.dotOverlay} />
-                <div ref={borderOverlayRef} className={styles.borderOverlay} />
-                <div ref={gridOverlayRef} className={styles.gridOverlay} data-active="false" />
+                <div className={styles.borderOverlay} />
+                <div className={styles.gridOverlay} />
             </div>
         </div>
     );
